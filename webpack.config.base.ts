8134812ed7695e15be1427fc91f65a6be1d04100
David Driscoll <david.driscoll@gmail.com>
2017-12-21 21:50:24 -0500

    omg it works

---
+++ webpack.config.base.ts
@@ -0,0 +1,77 @@
+import * as webpack from 'webpack';
+
+module.exports = <webpack.Configuration> {
+    output: {
+        filename: "./dist/[name].js",
+        //chunkFilename: "[chunkhash].js",
+        publicPath: './dist/',
+        pathinfo: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    preloaders: {
+                        // js: 'tslint-loader',
+                        ts: 'tslint-loader',
+                    },
+                    loaders: {
+                        // js: 'ts-loader',
+                        ts: 'ts-loader',
+                    }
+                }
+            },
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'ts-loader',
+                    options: {
+                        appendTsSuffixTo: [/\.vue$/]
+                    }
+                }, {
+                    loader: 'tslint-loader'
+                }]
+            }
+        ],
+    },
+    plugins: [],
+    resolve: {
+        extensions: ['.ts', '.tsx', '.vue', '.js', '.jsx', '.json', '.css', '.scss']
+    },
+    performance: {
+        hints: 'warning',
+
+    },
+    devtool: "source-map",
+    stats: {
+        colors: true,
+        // assets: true,
+        // cached: true,
+        // cachedAssets: true,
+        // children: true,
+        // chunkModules: true,
+        // chunkOrigins: true,
+        // chunks: true,
+        // env: true,
+        // errorDetails: true,
+        // errors: true,
+        // hash: true,
+        // modules: true,
+        // moduleTrace: true,
+        // performance: true,
+        // providedExports: true,
+        // publicPath: true,
+        // reasons: true,
+        // source: true,
+        // timings: true,
+        // usedExports: true,
+        // version: true,
+        // warnings: true,
+    },
+    // profile: true,
+    // cache: true,
+
+}
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,82 +1,28 @@
-import * as webpack from 'webpack';
+import * as webpack from "webpack";
+import * as config from './webpack.config.base';
+import { cloneDeep, merge } from 'lodash';
 
-module.exports = <webpack.Configuration> {
-    // entry: {
-    //     'main': './client/app.ts',
-    //     'server': './client/server.ts',
-    // },
-    target: "web",
-    output: {
-        filename: "./dist/[name].js",
-        //chunkFilename: "[chunkhash].js",
-        publicPath: './dist/',
-        pathinfo: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader',
-                options: {
-                    preloaders: {
-                        // js: 'tslint-loader',
-                        ts: 'tslint-loader',
-                    },
-                    loaders: {
-                        // js: 'ts-loader',
-                        ts: 'ts-loader',
-                    }
-                }
-            },
-            {
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-                use: [{
-                    loader: 'ts-loader',
-                    options: {
-                        appendTsSuffixTo: [/\.vue$/]
-                    }
-                }, {
-                    loader: 'tslint-loader'
-                }]
-            }
-        ],
-    },
-    plugins: [],
-    resolve: {
-        extensions: ['.ts', '.tsx', '.vue', '.js', '.jsx', '.json', '.css', '.scss']
-    },
-    performance: {
-        hints: 'warning',
+const server = merge(
+    cloneDeep(config),
+    <webpack.Configuration>{
+        target: "node",
+        entry: {
+            'server': './client/server.ts',
+        },
+        output: {
+            libraryTarget: 'commonjs'
+        }
+    }
+);
 
-    },
-    devtool: "source-map",
-    stats: {
-        colors: true,
-        // assets: true,
-        // cached: true,
-        // cachedAssets: true,
-        // children: true,
-        // chunkModules: true,
-        // chunkOrigins: true,
-        // chunks: true,
-        // env: true,
-        // errorDetails: true,
-        // errors: true,
-        // hash: true,
-        // modules: true,
-        // moduleTrace: true,
-        // performance: true,
-        // providedExports: true,
-        // publicPath: true,
-        // reasons: true,
-        // source: true,
-        // timings: true,
-        // usedExports: true,
-        // version: true,
-        // warnings: true,
-    },
-    // profile: true,
-    // cache: true,
+const client = merge(
+    cloneDeep(config),
+    {
+        target: "web",
+        entry: {
+            'main': './client/app.ts',
+        },
+    }
+);
 
-}
+module.exports = [server, client];
